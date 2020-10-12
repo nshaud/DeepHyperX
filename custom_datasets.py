fdbf0e7bef7678cc213e7b6dd3b86b85efdaa449
J.-F. Robitaille <jean-francois.robitaille@univ-grenoble-alpes.fr>
2020-10-12 11:44:22 +0200

    custom_datasets.py modified for astro

--- custom_datasets.py
+++ custom_datasets.py
@@ -1,45 +1,51 @@
-from utils import open_file
+from astropy.io import fits
 import numpy as np
 
 CUSTOM_DATASETS_CONFIG = {
-    "DFC2018_HSI": {
-        "img": "2018_IEEE_GRSS_DFC_HSI_TR.HDR",
-        "gt": "2018_IEEE_GRSS_DFC_GT_TR.tif",
+    "Taurus250_train": {
+        "img": "taurus_L1495_250_train.fits",
+        "gt": "taurus_L1495_250_train_mask.fits",
         "download": False,
-        "loader": lambda folder: dfc2018_loader(folder),
+        "loader": lambda folder: astro_loader("/Users/robitaij/postdoc/Lhyrica/Taurus/"),
+    },
+    "Taurus250_test": {
+        "img": "taurus_L1495_250_test.fits",
+        "gt": "taurus_L1495_250_test_mask.fits",
+        "download": False,
+        "loader": lambda folder: astro_loader("/Users/robitaij/postdoc/Lhyrica/Taurus/"),
     }
 }
 
 
-def dfc2018_loader(folder):
-    img = open_file(folder + "2018_IEEE_GRSS_DFC_HSI_TR.HDR")[:, :, :-2]
-    gt = open_file(folder + "2018_IEEE_GRSS_DFC_GT_TR.tif")
+def taurus_train_loader(folder):
+    img = fits.open(folder + "taurus_L1495_250_train.fits")[0].data[:, :, :-2]
+    gt = fits.open(folder + "taurus_L1495_250_train_mask.fits")[0].data
     gt = gt.astype("uint8")
 
     rgb_bands = (47, 31, 15)
 
     label_values = [
         "Unclassified",
-        "Healthy grass",
-        "Stressed grass",
-        "Artificial turf",
-        "Evergreen trees",
-        "Deciduous trees",
-        "Bare earth",
-        "Water",
-        "Residential buildings",
-        "Non-residential buildings",
-        "Roads",
-        "Sidewalks",
-        "Crosswalks",
-        "Major thoroughfares",
-        "Highways",
-        "Railways",
-        "Paved parking lots",
-        "Unpaved parking lots",
-        "Cars",
-        "Trains",
-        "Stadium seats",
+        "Starless",
+        "prestellar",
+        "protostellar",
+    ]
+    ignored_labels = [0]
+    palette = None
+    return img, gt, rgb_bands, ignored_labels, label_values, palette
+    
+def taurus_test_loader(folder):
+    img = fits.open(folder + "taurus_L1495_250_test.fits")[0].data[:, :, :-2]
+    gt = fits.open(folder + "taurus_L1495_250_test_mask.fits")[0].data
+    gt = gt.astype("uint8")
+
+    rgb_bands = (47, 31, 15)
+
+    label_values = [
+        "Unclassified",
+        "Starless",
+        "prestellar",
+        "protostellar",
     ]
     ignored_labels = [0]
     palette = None
